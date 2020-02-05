932148d26a0cbea7dc2a84f89d932fff6c8fe4f1
Denise Magner <deerenae@gmail.com>
2020-02-05 23:11:29 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,10 @@
 def square_array(array)
   counter = 0 
-  
+  new_array = []
   while array[counter] do
-    puts array[counter]
+    array.shift array[counter]**2
     counter += 1 
   end  
+  new_array
 end
 
-square_array(array)
